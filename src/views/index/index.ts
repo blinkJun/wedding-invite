9e078f6c531975a36aa716b3745cb78a2e66861c
blinkJun <530080147@qq.com>
2021-03-29 23:57:26 +0800

    首页动画

--- src/views/index/index.ts
+++ src/views/index/index.ts
@@ -1,4 +1,5 @@
 import "./style.scss";
+import "./ani.scss";
 
 // 设置初始化高度
 const bannerElement = document.querySelector('.banner') as HTMLDivElement
@@ -20,10 +21,60 @@ window.addEventListener('load',()=>{
     if(endTime-startTime<minDuration){
         setTimeout(()=>{
             closeLoading()
+            startElementInSight()
         },minDuration - (endTime-startTime))
     }else{
         closeLoading()
+        startElementInSight()
     }
 })
+
+const isInSight = function(img:HTMLElement) {
+    const bound = img.getBoundingClientRect();
+    const clientHeight = window.innerHeight;
+    //如果只考虑向下滚动加载
+    //const clientWidth = window.innerWeight;
+    return bound.top <= clientHeight - 100; // +100延迟加载
+}
+
+const initElAniState = (el:Element)=>{
+    if(!el.classList.contains('active-ani')){
+        el.classList.add('active-ani')
+    }
+}
+
+const startElementInSight = ()=>{
+    let allWillAniEl = Array.from(document.querySelectorAll('.ready-ani'));
+    // 两种监听方式
+    if ('IntersectionObserver' in window) {
+        const ob = new IntersectionObserver((changes) => {
+            for (const change of changes) {
+                if (0 < change.intersectionRatio && change.intersectionRatio <= 1) {
+                    initElAniState(change.target);
+                    ob.unobserve(change.target);
+                }
+            }
+        })
+        allWillAniEl.forEach((el:Element) => {
+            ob.observe(el);
+        })
+    } else {
+        window.addEventListener('scroll',()=>{
+            allWillAniEl.forEach((el:HTMLElement) => {
+                if (isInSight(el)) {
+                    initElAniState(el);
+                }
+            })
+        })
+    }
+
+    // 直接进行一次判断
+    allWillAniEl.forEach((el:HTMLElement) => {
+        if (isInSight(el)) {
+            initElAniState(el);
+        }
+    })
+}
+
 
 
