76025463325467773167e63318fb7f07db1df0b1
blinkJun <530080147@qq.com>
2021-03-29 22:01:25 +0800

    部署和基本排版

--- src/views/index/index.ts
+++ src/views/index/index.ts
@@ -5,25 +5,25 @@ const bannerElement = document.querySelector('.banner') as HTMLDivElement
 bannerElement.style.height = window.innerHeight + 'px'
 const startTime = Date.now()
 
-// const closeLoading = function(){
-//     const loadingEl = document.querySelector('.loading') as HTMLDivElement
-//     loadingEl.classList.add('hide')
-//     setTimeout(()=>{
-//         loadingEl.remove()
-//     },200)
-// }
+const closeLoading = function(){
+    const loadingEl = document.querySelector('.loading') as HTMLDivElement
+    loadingEl.classList.add('hide')
+    setTimeout(()=>{
+        loadingEl.remove()
+    },200)
+}
 
-// // 资源加载完成时移除loading
-// window.addEventListener('load',()=>{
-//     const endTime = Date.now()
-//     const minDuration = 2000
-//     if(endTime-startTime<minDuration){
-//         setTimeout(()=>{
-//             closeLoading()
-//         },minDuration - (endTime-startTime))
-//     }else{
-//         closeLoading()
-//     }
-// })
+// 资源加载完成时移除loading
+window.addEventListener('load',()=>{
+    const endTime = Date.now()
+    const minDuration = 2000
+    if(endTime-startTime<minDuration){
+        setTimeout(()=>{
+            closeLoading()
+        },minDuration - (endTime-startTime))
+    }else{
+        closeLoading()
+    }
+})
 
 
