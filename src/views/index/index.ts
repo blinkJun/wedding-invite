2e4fb228d83984ee1c61a4d043e8832e7be6195e
liangjun <530080147@qq.com>
2021-03-30 14:32:33 +0800

    微信分享

--- src/views/index/index.ts
+++ src/views/index/index.ts
@@ -1,5 +1,8 @@
 import "./style.scss";
 import "./ani.scss";
+import axios from 'axios'
+const wx = require('weixin-js-sdk')
+
 
 // 设置初始化高度
 const bannerElement = document.querySelector('.banner') as HTMLDivElement
@@ -75,6 +78,52 @@ const startElementInSight = ()=>{
         }
     })
 }
+
+axios.get(`http://sunrise.tojike.com/hawk-api/wechat/signature?url=${encodeURIComponent(location.href)}`).then((res)=>{
+    const {jsapi_ticket,noncestr,signature,timestamp} = res.data.data
+    wx.config({
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: 'wx0a6c14eccd76ea7a', // 必填，公众号的唯一标识
+        timestamp: timestamp, // 必填，生成签名的时间戳
+        nonceStr: noncestr, // 必填，生成签名的随机串
+        signature: signature,// 必填，签名
+        jsApiList: [
+            'updateTimelineShareData',
+            'updateAppMessageShareData',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+        ] // 必填，需要使用的JS接口列表
+    });
+})
+wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
+    const title = '梁俊和韦晓霞的婚礼'
+    const link = location.href
+    const imageUrl = 'http://sunrise.tojike.com/wedding/img/HRQ11517.08741350.jpg'
+    const desc = '欢迎您来参加我们的婚礼'
+
+    wx.updateTimelineShareData({ 
+      title:title , // 分享标题
+      link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl:imageUrl , // 分享图标
+    })
+    wx.updateAppMessageShareData({ 
+        title: title, // 分享标题
+        desc: desc, // 分享描述
+        link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: imageUrl, // 分享图标
+    })
+    wx.onMenuShareTimeline({
+        title: title, // 分享标题
+        link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: imageUrl, // 分享图标
+    })
+    wx.onMenuShareAppMessage({
+        title: title, // 分享标题
+        desc: desc, // 分享描述
+        link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: imageUrl // 分享图标
+    });
+}); 
 
 
 
